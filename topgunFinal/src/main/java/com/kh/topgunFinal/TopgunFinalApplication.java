56bbdb3f21c537e92c8106f185613e194813b425
parkjunho2 <pp01024689860@gmail.com>
2024-09-27 21:04:53 +0900

    파이널 프로젝트 생성

---
+++ topgunFinal/src/main/java/com/kh/topgunFinal/TopgunFinalApplication.java
@@ -0,0 +1,13 @@
+package com.kh.topgunFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopgunFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopgunFinalApplication.class, args);
+	}
+
+}
---
+++ topgunFinal/src/test/java/com/kh/topgunFinal/TopgunFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.topgunFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopgunFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
