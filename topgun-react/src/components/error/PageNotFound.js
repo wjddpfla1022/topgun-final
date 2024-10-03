30370636fd84b3294526d49fcd2b20cecebdf562
parkjunho2 <pp01024689860@gmail.com>
2024-10-03 23:26:51 +0900

    route and route-dom installing

--- topgun-react/src/components/error/PageNotFound.js
+++ topgun-react/src/components/error/PageNotFound.js
@@ -1,14 +1,15 @@
 import {useNavigate} from "react-router";
 
 const PageNotFound=()=>{
-    //navigate
+
     const navigate= useNavigate();
 
     return (<>
     <div className="row mt-4">
         <div className="col">
             <h2>요청하신 페이지를 찾을 수 없습니다.</h2>
-            <button type="button" className="btn btn-secondary">
+            <button type="button" className="btn btn-secondary"
+                onClick={e=>navigate(-1)}>
             뒤로가기
             </button>
         </div>
