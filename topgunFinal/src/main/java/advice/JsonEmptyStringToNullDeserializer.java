34384510b0d187331116cb90c609369ee24dc064
parkjunho2 <pp01024689860@gmail.com>
2024-10-07 21:15:50 +0900

    jsonEptyStringtonull 추가

---
+++ topgunFinal/src/main/java/advice/JsonEmptyStringToNullDeserializer.java
@@ -0,0 +1,19 @@
+package advice;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+//JSON 내부에 빈 문자열을 null로 치환하는 도구(등록 필요 없고 DTO, VO에 선언)
+public class JsonEmptyStringToNullDeserializer extends JsonDeserializer<String>{
+	@Override
+	public String deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+		//JsonParser가 @JsonDeserialize가 붙은 필드 혹은 클래스의 내용을 읽는다
+		String value = p.getText();
+		boolean empty = value == null || value.isEmpty();
+		return empty ? null : value;
+	}
+}
