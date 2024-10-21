4d94bb65d35b24ea60464192362d3d1311c79ce0
IhubeI <jkl5914@naver.com>
2024-10-21 15:17:16 +0900

    요것도저장

--- topgun-react/src/components/Route/PrivateRoute.js
+++ topgun-react/src/components/Route/PrivateRoute.js
@@ -1,6 +1,7 @@
 import { useRecoilValue } from "recoil";
 import { Navigate, Outlet } from "react-router-dom";
 import { loginState, memberLoadingState } from "../../util/recoil";
+import Oval from "react-loading-icons/dist/esm/components/oval";
 
 const PrivateRoute = () => {
     // 로그인 검사 결과를 불러온다
@@ -9,7 +10,11 @@ const PrivateRoute = () => {
 
     // 로딩 진행중이라면 로딩 화면을 보여준다
     if (memberLoading === false) {
-        return <h1>Loading...</h1>;
+        return (<>
+            <div className="d-flex justify-content-center align-items-center" style={{ height: '70vh' }}>
+                <Oval stroke="#007bff" /> {/* Oval 로딩 아이콘 */}
+            </div>
+        </>);
     }
 
     // 로그인 상태에 따라 Outlet을 렌더링
