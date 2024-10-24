f90f6a9ca774421940f02c7c3c1447dc319aa31a
parkjunho2 <pp01024689860@gmail.com>
2024-10-24 17:52:13 +0900

    좌석정보 업데이트

--- topgunFinal/src/main/java/com/kh/topgunFinal/dto/PaymentDetailDto.java
+++ topgunFinal/src/main/java/com/kh/topgunFinal/dto/PaymentDetailDto.java
@@ -6,9 +6,18 @@ import lombok.Data;
 public class PaymentDetailDto {
     private int paymentDetailNo;
     private String paymentDetailName;
+    private String paymentDetailRank;
     private int paymentDetailPrice;
     private int paymentDetailQty;
     private int paymentDetailSeatsNo;
     private int paymentDetailOrigin;
     private String paymentDetailStatus;
+    private String paymentDetailPassport;
+    private String paymentDetailPassanger;
+    private String paymentDetailEnglish;
+    private String paymentDetailSex;
+    private String paymentDetailBirth;
+    private String paymentDetailCountry;
+    private String paymentDetailVisa;
+    private String paymentDetailExpire;
 }
