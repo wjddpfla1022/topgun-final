ee6e272ed29c60080c3149b1a9a9c1278f8082f1
parkjunho2 <pp01024689860@gmail.com>
2024-10-28 11:11:58 +0900

    결제DB 저장 취소 상세페이지 완료

--- topgunFinal/src/main/java/com/kh/topgunFinal/dto/PaymentDetailDto.java
+++ topgunFinal/src/main/java/com/kh/topgunFinal/dto/PaymentDetailDto.java
@@ -9,19 +9,19 @@ public class PaymentDetailDto {
     private int paymentDetailNo;
     private int flightId;
     private String paymentDetailName;
-    private String paymentDetailRank;
+//    private String paymentDetailRank;
     private int paymentDetailPrice;
     private int paymentDetailQty;
     private int paymentDetailSeatsNo;
     private int paymentDetailOrigin;
     private String paymentDetailStatus;
-    private String paymentDetailPassport;
-    private String paymentDetailPassanger;
-    private String paymentDetailEnglish;
-    private String paymentDetailSex;
-    private LocalDate paymentDetailBirth;
-    private String paymentDetailCountry;
-    private String paymentDetailVisa;
-    private LocalDate paymentDetailExpire;
+//    private String paymentDetailPassport;
+//    private String paymentDetailPassanger;
+//    private String paymentDetailEnglish;
+//    private String paymentDetailSex;
+//    private LocalDate paymentDetailBirth;
+//    private String paymentDetailCountry;
+//    private String paymentDetailVisa;
+//    private LocalDate paymentDetailExpire;
 }
  
