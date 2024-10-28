b4489e5bfd5a3b2d79e4ca781fc4c3255e95f62a
dmstn12 <supreme741@naver.com>
2024-10-28 18:11:57 +0900

    복합검색 처리를 위한 코드 업데이트(인원수는 미구현..)

--- topgunFinal/src/main/java/com/kh/topgunFinal/vo/FlightComplexSearchRequestVO.java
+++ topgunFinal/src/main/java/com/kh/topgunFinal/vo/FlightComplexSearchRequestVO.java
@@ -1,16 +1,29 @@
 package com.kh.topgunFinal.vo;
 
+import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.kh.topgunFinal.advice.JsonEmptyStringToNullDeserializer;
+
 import lombok.Data;
 
 //Request VO (복합 검색 요청)
 @Data
 public class FlightComplexSearchRequestVO {
 	    private String departureAirport;  // 출발지
+	    
 	    private String arrivalAirport;    // 도착지
-	    private Timestamp departureTime;     // 출발 날짜 (YYYY-MM-DD)
-	    private int passengers;       // 인원수
+	    
+	    private String airlineName;
+		
+	    @JsonDeserialize(using = JsonEmptyStringToNullDeserializer.class)
+	    private String departureTime;     // 출발 날짜 (YYYY-MM-DD)
+	    
+	    private Integer passengers;       // 인원수
+	    
 	    private List<String> orderList;   // 정렬 기준
 	    private Integer beginRow, endRow;
 
